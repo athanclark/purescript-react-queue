10cddd205fd74d4e9050d118402312b6c846f209
Athan Clark <athan.clark@gmail.com>
2019-03-09 22:55:53 -0700

    actually creates a context, doesn't bind to a new one

--- src/React/Signal/WhileMounted.purs
+++ src/React/Signal/WhileMounted.purs
@@ -3,8 +3,8 @@ module React.Signal.WhileMounted where
 import Prelude (Unit, class Eq, bind, discard, ($), pure, unit)
 import Effect (Effect)
 import React
-  ( ReactClassConstructor, ReactThis, ComponentDidMount, ComponentWillUnmount, ContextProvider, ReactElement, ReactClass, ReactSpecRequired
-  , createLeafElement, createElement, component, setState, getState)
+  ( ReactClassConstructor, ReactThis, ComponentDidMount, ComponentWillUnmount, ReactElement, ReactClass, ReactSpecRequired
+  , createLeafElement, createElement, component, setState, getState, createContext)
 import Signal.Types (READ)
 import Signal (Signal, subscribeLight, clear, get) as Signal
 import IxSignal (IxSignal, subscribeLight, subscribeDiffLight, delete, get) as IxSignal
@@ -67,92 +67,110 @@ whileMountedIxDiff sig k f constructor = \this -> do
     }
 
 
-
+-- | Provider takes the component name as the first argument
 signalToProvider :: forall a
                   . Signal.Signal (read :: READ) a
-                 -> ContextProvider a
-                 -> String -- ^ Component name
-                 -> Array ReactElement
-                 -> ReactElement
-signalToProvider sig prov name children = createLeafElement c {}
-  where
-    c :: ReactClass {}
-    c = component name constructor
-      where
-        constructor :: ReactClassConstructor {} {value :: a} (Mounted (ReactSpecRequired {value :: a} ()))
-        constructor =
-          let handleValueChange :: ReactThis {} {value :: a} -> a -> Effect Unit
-              handleValueChange this value = setState this {value}
-          in  whileMounted sig handleValueChange constructor'
-          where
-            constructor' this = do
-              initValue <- Signal.get sig
-              pure
-                { state: {value: initValue}
-                , render: do
-                    {value} <- getState this
-                    pure (createElement prov {value} children)
-                , componentDidMount: pure unit
-                , componentWillUnmount: pure unit
-                }
+                 -> Effect
+                      { consumer :: (a -> ReactElement) -> ReactElement
+                      , provider :: String -> Array ReactElement -> ReactElement
+                      }
+signalToProvider sig = do
+  initValue <- Signal.get sig
+  let {consumer,provider} = createContext initValue
+  pure
+    { consumer: \f -> createLeafElement consumer {children: f}
+    , provider: \name children ->
+      let c :: ReactClass {}
+          c = component name constructor
+            where
+              constructor :: ReactClassConstructor {} {value :: a} (Mounted (ReactSpecRequired {value :: a} ()))
+              constructor =
+                let handleValueChange :: ReactThis {} {value :: a} -> a -> Effect Unit
+                    handleValueChange this value = setState this {value}
+                in  whileMounted sig handleValueChange constructor'
+                where
+                  constructor' this = do
+                    initValue' <- Signal.get sig
+                    pure
+                      { state: {value: initValue'}
+                      , render: do
+                          {value} <- getState this
+                          pure (createElement provider {value} children)
+                      , componentDidMount: pure unit
+                      , componentWillUnmount: pure unit
+                      }
+      in  createLeafElement c {}
+    }
 
 
+-- | Provider takes the index of the signal to bind to as the first argument, the second is the component name
 ixSignalToProvider :: forall a
                     . IxSignal.IxSignal (read :: READ) a
-                   -> ContextProvider a
-                   -> String -- ^ Signal Index
-                   -> String -- ^ Component name
-                   -> Array ReactElement
-                   -> ReactElement
-ixSignalToProvider sig prov ix name children = createLeafElement c {}
-  where
-    c :: ReactClass {}
-    c = component name constructor
-      where
-        constructor :: ReactClassConstructor {} {value :: a} (Mounted (ReactSpecRequired {value :: a} ()))
-        constructor =
-          let handleValueChange :: ReactThis {} {value :: a} -> a -> Effect Unit
-              handleValueChange this value = setState this {value}
-          in  whileMountedIx sig ix handleValueChange constructor'
-          where
-            constructor' this = do
-              initValue <- IxSignal.get sig
-              pure
-                { state: {value: initValue}
-                , render: do
-                    {value} <- getState this
-                    pure (createElement prov {value} children)
-                , componentDidMount: pure unit
-                , componentWillUnmount: pure unit
-                }
+                   -> Effect
+                        { consumer :: (a -> ReactElement) -> ReactElement
+                        , provider :: String -> String -> Array ReactElement -> ReactElement
+                        }
+ixSignalToProvider sig = do
+  initValue <- IxSignal.get sig
+  let {consumer,provider} = createContext initValue
+  pure
+    { consumer: \f -> createLeafElement consumer {children: f}
+    , provider: \ix name children ->
+      let c :: ReactClass {}
+          c = component name constructor
+            where
+              constructor :: ReactClassConstructor {} {value :: a} (Mounted (ReactSpecRequired {value :: a} ()))
+              constructor =
+                let handleValueChange :: ReactThis {} {value :: a} -> a -> Effect Unit
+                    handleValueChange this value = setState this {value}
+                in  whileMountedIx sig ix handleValueChange constructor'
+                where
+                  constructor' this = do
+                    initValue' <- IxSignal.get sig
+                    pure
+                      { state: {value: initValue'}
+                      , render: do
+                          {value} <- getState this
+                          pure (createElement provider {value} children)
+                      , componentDidMount: pure unit
+                      , componentWillUnmount: pure unit
+                      }
+      in  createLeafElement c {}
+    }
 
 
+-- | Provider takes the index of the signal to bind to as the first argument, the second is the component name
 ixSignalDiffToProvider :: forall a
                         . Eq a
                        => IxSignal.IxSignal (read :: READ) a
-                       -> ContextProvider a
-                       -> String -- ^ Signal Index
-                       -> String -- ^ Component name
-                       -> Array ReactElement
-                       -> ReactElement
-ixSignalDiffToProvider sig prov ix name children = createLeafElement c {}
-  where
-    c :: ReactClass {}
-    c = component name constructor
-      where
-        constructor :: ReactClassConstructor {} {value :: a} (Mounted (ReactSpecRequired {value :: a} ()))
-        constructor =
-          let handleValueChange :: ReactThis {} {value :: a} -> a -> Effect Unit
-              handleValueChange this value = setState this {value}
-          in  whileMountedIxDiff sig ix handleValueChange constructor'
-          where
-            constructor' this = do
-              initValue <- IxSignal.get sig
-              pure
-                { state: {value: initValue}
-                , render: do
-                    {value} <- getState this
-                    pure (createElement prov {value} children)
-                , componentDidMount: pure unit
-                , componentWillUnmount: pure unit
-                }
+                       -> Effect
+                            { consumer :: (a -> ReactElement) -> ReactElement
+                            , provider :: String -> String -> Array ReactElement -> ReactElement
+                            }
+ixSignalDiffToProvider sig = do
+  initValue <- IxSignal.get sig
+  let {consumer,provider} = createContext initValue
+  pure
+    { consumer: \f -> createLeafElement consumer {children: f}
+    , provider: \ix name children ->
+      let c :: ReactClass {}
+          c = component name constructor
+            where
+              constructor :: ReactClassConstructor {} {value :: a} (Mounted (ReactSpecRequired {value :: a} ()))
+              constructor =
+                let handleValueChange :: ReactThis {} {value :: a} -> a -> Effect Unit
+                    handleValueChange this value = setState this {value}
+                in  whileMountedIxDiff sig ix handleValueChange constructor'
+                where
+                  constructor' this = do
+                    initValue' <- IxSignal.get sig
+                    pure
+                      { state: {value: initValue'}
+                      , render: do
+                          {value} <- getState this
+                          pure (createElement provider {value} children)
+                      , componentDidMount: pure unit
+                      , componentWillUnmount: pure unit
+                      }
+      in  createLeafElement c {}
+    }
